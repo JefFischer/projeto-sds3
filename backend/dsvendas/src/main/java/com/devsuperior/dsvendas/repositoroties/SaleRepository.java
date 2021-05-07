7c992f320f6ae3351a9e33665bf979fa5e53e464
JefFischer <cffischer100@gmail.com>
2021-05-06 21:40:36 -0300

    Pagination

---
+++ backend/dsvendas/src/main/java/com/devsuperior/dsvendas/repositoroties/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsvendas.repositoroties;
+
+import com.devsuperior.dsvendas.entities.Sale;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
